c287d58879440a33aced7d5f249d5477c9b2e1da
Lawrence Jones <lawrencejones1234@icloud.com>
2024-04-29 23:57:46 -0500

    Act2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'home', to: 'pages#home'
+  root to: redirect('/home')
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'about', to: 'pages#about'
+
+  get 'work', to: 'pages#work'
+
+  get 'contact', to: 'pages#contact'
+
 end
